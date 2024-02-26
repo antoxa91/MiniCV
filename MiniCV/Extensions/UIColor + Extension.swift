d7e855b133e4a0fb0290674c2fc21d7429b372bf
Anton Stafeev <stafeevt@mail.ru>
2024-02-26 22:07:25 +0500

    Add extensions

---
+++ MiniCV/Extensions/UIColor + Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor + Extension.swift
+//  MiniCV
+//
+//  Created by Антон Стафеев on 26.02.2024.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach({
+            addSubview($0)
+        })
+    }
+}
