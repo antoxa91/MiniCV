ec0c8632936d93809559aa0607fcb5c5f74fbac1
Anton Stafeev <stafeevt@mail.ru>
2024-02-26 21:52:27 +0500

    Initial Commit

---
+++ MiniCV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniCV
+//
+//  Created by Антон Стафеев on 26.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
